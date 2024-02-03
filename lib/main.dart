02b2b7c2cf1e87b72b1275d7b16d0fd6f1819123
nilufar786 <nilufarmikrani786@gmail.com>
2024-02-04 00:46:21 +0545

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class QRScannerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: QRScannerScreen(),
     );
   }
